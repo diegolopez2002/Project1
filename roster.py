8ddb2120dbe14dc4ba0acd3ef4576b053ec81159
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-08 12:14:08 -0400

    Update roster.py

--- roster.py
+++ roster.py
@@ -55,7 +55,7 @@ class Staff(Person):
         return self.position
 
 
-class Roster:
+class Roster():
 
 
     def __init__(self):
@@ -87,8 +87,10 @@ class Roster:
         return self.name
 
         
-    def map():
+    def map(Person):
         pass
+        
+        
 
 
 
