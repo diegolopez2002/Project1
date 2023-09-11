6e318ad498f35250211d33d8596532a4177b55f8
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-09-10 22:18:56 -0400

    Update basics.py

--- basics.py
+++ basics.py
@@ -55,6 +55,9 @@ def zipHash(arr1, arr2):
      if len(arr1) != len(arr2):
           return None
 
+     if len(arr1) == 0 or len(arr2) == 0:
+          return None
+
      for i in range(len(arr1)):
           
           if arr1[i] is None or arr2[i] is None:
